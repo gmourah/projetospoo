1b12011f874518d5db34f88ebf89e587ef1dae03
gilva <gilva@Gilvan-Mourah>
2022-06-15 19:08:14 -0300

    TrabalhoFinalUpgrade1

--- ProjetoFinal/src/ProjetoFInal/Aluno.java
+++ ProjetoFinal/src/ProjetoFInal/Aluno.java
@@ -1,31 +1,50 @@
 package ProjetoFInal;
 
-public class Aluno{
-
-	//Atributos
-    private int matricula;
-
-    //Métodos Personalizados
-    public void matricular(){
-        System.out.println("Aluno(a): " +getNome()+ " Matriculado(a) com sucesso!");
-    }
-    
-    //Métodos Especiais
-    public Aluno(int matricula, String nome, String sexo, int idade, String cpf, int aingresso) {
-        super(nome, sexo, idade, cpf, aingresso);
-        this.matricula = matricula;
-    }
-    
-    public int getMatricula() {
-        return matricula;
-    }
-
-    public void setMatricula(int matricula) {
-        this.matricula = matricula;
-    }
-
-    public String toString() {
-        return "------------------ \nALUNO(A): " + getNome() + "\nIDADE: "+ getIdade() + "\nSEXO: " + getSexo()+
-                "\nNº DO CPF: " + getCpf()+ "\nNº DE MATRÍCULA: " + getMatricula()+ "Ano Ingresso: "+ getAingresso() +"\n";
-    }
-}
+public class Aluno {
+	private String matricula;
+	public String nome; 
+	public String sexo; 
+	public String idade;
+	public int aingresso;
+	
+	public Aluno(String m, String n, String s, String idd, int a ) {
+	this.matricula = m;	
+	this.nome = n;
+	this.sexo = s;
+	this.idade = idd;
+	this.aingresso = a;
+	}
+	
+	public String getMatricula() {
+		return this.matricula;
+	}
+	public void setMatricula(String m) {
+		this.matricula = m;
+	}
+	public String getNome() {
+		return this.nome;
+	}
+	public void setNome(String n) {
+		this.nome = n;
+	}
+	public String getSexo() {
+		return this.sexo;
+	}
+	public void setSexo(String s) {
+		this.sexo = s;
+	}
+	public String getIdade() {
+		return this.idade;
+	}
+	public void setIdade(String idd) {
+		this.idade = idd;
+	}
+	public int getAingresso() {
+		return this.aingresso;
+	}
+	public void setAingresso(int a) {
+		this.aingresso = a;
+	}
+	
+	}
+	
